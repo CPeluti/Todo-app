ebee11fe8ca9a20f8a1db06ee8fa7e21faa605de
AnaJordao <acjpjvjp@gmail.com>
2021-04-08 17:37:54 -0300

    backup do botao nova categoria

--- src/app/controllers/UserController.java
+++ src/app/controllers/UserController.java
@@ -1,9 +1,52 @@
-package app.controllers;
+package controllers;
 
-import app.models.User;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
 
-import java.util.ArrayList;
+public class UserController {
 
-public class UserController{
-    public static User usuario = new User();
+    @FXML
+    private Label btnNewCategory;
+
+    @FXML
+    private Label lbMsgMakeCategory;
+
+    @FXML
+    private AnchorPane pnNewCategory;
+
+    @FXML
+    private TextField txtCategoryName;
+
+    @FXML
+    private TextArea txtCategoryDescription;
+
+    @FXML
+    private Button btnSaveNewCategory;
+
+    @FXML
+    void showMsgMakeCategory(MouseEvent event) {    // When the mouse enters the button, it shows the message "Criar nova categoria"
+        if(!pnNewCategory.isVisible()){
+            lbMsgMakeCategory.setVisible(true);
+        }
+    }
+
+    @FXML
+    void btnMakeCategory(MouseEvent event) {
+        pnNewCategory.setVisible(true);
+    }
+
+    @FXML
+    void saveNewCategory(ActionEvent event) {
+
+        String nameCategory = txtCategoryName.getText();
+        String descriptionCategory = txtCategoryDescription.getText();
+
+        pnNewCategory.setVisible(false);
+    }
 }
