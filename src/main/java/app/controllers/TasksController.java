dc1602f94408429720ee1afb252b6bbadc003f2b
Luiza Gomes <luizangomes@outlook.com>
2021-04-11 18:21:27 -0300

    Novas partes (json), novo esquema de cores

--- src/main/java/app/controllers/TasksController.java
+++ src/main/java/app/controllers/TasksController.java
@@ -4,16 +4,17 @@ import app.models.Tasks;
 import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
 import javafx.scene.control.DatePicker;
-import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
+import org.json.JSONObject;
+
 import java.io.FileWriter;
 import java.io.IOException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 
 public class TasksController {
-    public RadioButton favouriteId;
     public DatePicker dateId;
     public TextField taskId;
     public TextField categoryId;
@@ -22,37 +23,39 @@ public class TasksController {
     public Button savingTask;
     public TextField descriptionId;
     public AnchorPane bg;
+    public TextField time;
 
     public void newTaskBoxOpen(ActionEvent actionEvent) {
         taskCreator.setVisible(true);
     }
 
-    public void amIFavourite(ActionEvent actionEvent) {
-        favouriteId.setSelected(true);
-    }
 
     public void saveTask(ActionEvent actionEvent) {
-        try {
-            FileWriter writetofile = new FileWriter("Tasks.txt", true);
-            DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-            LocalDate date = dateId.getValue();
-            String dateofthistask = date.format(dateFormat);
-            String titleofthistask = taskId.getText();
-            String categoryofthistask = categoryId.getText();
-            String descriptionofthistask = descriptionId.getText();
-            Boolean isfavouriteofthistask = (Boolean) favouriteId.isSelected();
-            Tasks task = new Tasks(titleofthistask, categoryofthistask, false, isfavouriteofthistask, dateofthistask, descriptionofthistask);
-            writetofile.write(titleofthistask + "-");
-            writetofile.write(categoryofthistask + "-");
-            writetofile.write(String.valueOf(false) + "-");
-            writetofile.write(String.valueOf(isfavouriteofthistask) + "-");
-            writetofile.write(dateofthistask + "-");
-            writetofile.write(descriptionofthistask + "-");
-            writetofile.close();
+        DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        LocalDate date = dateId.getValue();
+        String timeofthistask = time.getText();
+        String datentimeofthistask = date.format(dateFormat) + " " + timeofthistask;
+        String titleofthistask = taskId.getText();
+        String categoryofthistask = categoryId.getText();
+        String descriptionofthistask = descriptionId.getText();
+        Tasks task = new Tasks(titleofthistask, categoryofthistask, false, false, datentimeofthistask, descriptionofthistask);
+        ArrayList<String> newTaskArray = new ArrayList<String>();
+        newTaskArray.add(task.title);
+        newTaskArray.add(task.category);
+        newTaskArray.add(String.valueOf(task.done));
+        newTaskArray.add(String.valueOf(task.favourite));
+        newTaskArray.add(task.deadline);
+        newTaskArray.add(task.description);
+        JSONObject newTaskObject = new JSONObject();
+        newTaskObject.put("Task:", newTaskArray);
+        try (FileWriter file = new FileWriter("tasksList.json", true)) {
+            file.write(newTaskObject.toString());
+            file.flush();
+
         } catch (IOException e) {
-            System.out.println("An error occurred.");
             e.printStackTrace();
         }
+
         taskCreator.setVisible(false);
     }
 }
