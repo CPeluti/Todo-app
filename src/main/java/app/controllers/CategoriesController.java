914e5c83e4c8a86f711c5fb32adfcc703dc17ebf
AnaJordao <acjpjvjp@gmail.com>
2021-04-22 19:09:00 -0300

    começo de trabalho com icones

--- src/main/java/app/controllers/CategoriesController.java
+++ src/main/java/app/controllers/CategoriesController.java
@@ -1,9 +1,6 @@
 package app.controllers;
 
 import app.models.UserCategory;
-import de.jensd.fx.glyphs.GlyphIcon;
-import de.jensd.fx.glyphs.fontawesome.FontAwesomeIcon;
-import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconView;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
@@ -14,6 +11,7 @@ import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Priority;
+import javafx.scene.shape.SVGPath;
 import org.json.JSONObject;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -60,13 +58,21 @@ public class CategoriesController {
     private TextField txtIcon;
     private String URIImage;
 
+    private SVGPath iconSvg;
+
     @FXML
     private AnchorPane pnIcons;
 
+    @FXML
+    private ScrollPane spIcons;
+
+    @FXML
+    private Button btnCancelIcon;
 
 
     public void initialize(){
         pnNewCategory.setVisible(false);
+        pnIcons.setVisible(false);
     }
 
     @FXML
@@ -100,16 +106,17 @@ public class CategoriesController {
         for(UserCategory usercategory : listUserCategories){
 
 
-            String filePath = URIImage;
-            Image icon = new Image(filePath, 35, 35, false, false);
-            ImageView imageView = new ImageView(icon);
-            imageView.setPreserveRatio(true);
+            SVGPath selectedIcon = iconSvg;
 
-            gpNavBar.add(new ImageView(icon), column, row);
+            //Image icon = new Image(filePath, 35, 35, false, false);
+            //ImageView imageView = new ImageView(icon);
+            //imageView.setPreserveRatio(true);
+
+            gpNavBar.add(selectedIcon, column, row);
             row++;
 
             // event that occurs when the icon is clicked
-            imageView.setOnMouseClicked((e) -> {
+            selectedIcon.setOnMouseClicked((e) -> {
                 // show the tasks of the respective category
             });
         }
@@ -176,26 +183,46 @@ public class CategoriesController {
     }
 
     @FXML
+    void cancelIcon(ActionEvent event) {
+
+        pnIcons.setVisible(false);
+    }
+
+    @FXML
     void selectIcon(ActionEvent event) {
 
-        //FileChooser fileChooser = new FileChooser();
-        //FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("Image Files", "*.png", "*.jpg");
-        //fileChooser.getExtensionFilters().add(extFilter);
-        //fileChooser.setTitle("Escolha um ícone para a nova categoria");
 
-        //File file = fileChooser.showOpenDialog(pnNewCategory.getScene().getWindow());
+        //GridPane gpIcons = new GridPane();
+        //gpIcons.setVgap(10);
+        //ColumnConstraints columnIcon = new ColumnConstraints(10, 100, 100);
+        //columnIcon.setHgrow(Priority.ALWAYS);
+        //gpIcons.getColumnConstraints().addAll(columnIcon, columnIcon, columnIcon, columnIcon, columnIcon, columnIcon);
+        //int row = 0;
+        //int column = 0;
 
-        //if (file != null) {
-        //    this.txtIcon.setText(file.toString());
-        //    URIImage = file.toURI().toString();
-        //}
+        // Icon 1
+        //SVGPath icon = new SVGPath();
+        //icon.setContent("M436 160c6.6 0 12-5.4 12-12v-40c0-6.6-5.4-12-12-12h-20V48c0-26.5-21.5-48-48-48H48C21.5 0 0 21.5 0 48v416c0 26.5 21.5 48 48 48h320c26.5 0 48-21.5 48-48v-48h20c6.6 0 12-5.4 12-12v-40c0-6.6-5.4-12-12-12h-20v-64h20c6.6 0 12-5.4 12-12v-40c0-6.6-5.4-12-12-12h-20v-64h20zm-68 304H48V48h320v416zM208 256c35.3 0 64-28.7 64-64s-28.7-64-64-64-64 28.7-64 64 28.7 64 64 64zm-89.6 128h179.2c12.4 0 22.4-8.6 22.4-19.2v-19.2c0-31.8-30.1-57.6-67.2-57.6-10.8 0-18.7 8-44.8 8-26.9 0-33.4-8-44.8-8-37.1 0-67.2 25.8-67.2 57.6v19.2c0 10.6 10 19.2 22.4 19.2z");
 
-        pnIcons.setVisible(true);
+        //gpIcons.add(icon, column, row);
+
+        //icon.setOnMouseClicked((e) -> {
+        //    iconSvg = icon;
+        //});
 
 
-        GlyphIcon<FontAwesomeIcon> selectedIcon1 = new FontAwesomeIconView(FontAwesomeIcon.TOGGLE_ON);
-        selectedIcon1.setSize("1.5em");
-        GlyphIcon<FontAwesomeIcon> notSelectedIcon1 = new FontAwesomeIconView(FontAwesomeIcon.TOGGLE_OFF);
-        notSelectedIcon1.setSize("1.5em");
+
+
+        //this.spIcons.setStyle("-fx-background: #571da8; -fx-border-color: #571da8");
+        //this.spIcons.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+        //this.spIcons.setVbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+
+        //this.spIcons.setContent(gpIcons);
+
+        //pnIcons.setVisible(true);
+        //spIcons.setVisible(true);
+
     }
+
+
 }
