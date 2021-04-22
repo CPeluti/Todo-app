5bff0a77e0ec43a67113203a55e605d16346bb82
Luiza N Gomes <luizangomes@outlook.com>
2021-04-22 19:08:27 -0300

    Atualização do dia 22/04

--- src/main/java/app/controllers/TasksController.java
+++ src/main/java/app/controllers/TasksController.java
@@ -10,13 +10,13 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import org.json.JSONObject;
 
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
-
-import static app.controllers.UserController.listUserCategories;
+import java.time.temporal.TemporalAccessor;
+import java.util.Date;
 
 public class TasksController {
     public DatePicker dateId;
@@ -26,11 +26,26 @@ public class TasksController {
     public TextField descriptionId;
     public AnchorPane bg;
     public TextField time;
-    public ComboBox categoryId = new ComboBox(FXCollections.observableArrayList(listUserCategories));
     public Button closeTaskCreator;
+    String[] list = {"TP1", "MUSIC", "CN"};
+    public ComboBox categoryId;
+    public void initialize() {
+        categoryId.getItems().addAll(FXCollections.observableArrayList(list));
+    }
+    private JSONObject newTaskObject = new JSONObject();
 
+    public TasksController() throws FileNotFoundException {
+    }
 
-    public void saveTask(ActionEvent actionEvent) {
+    public void saveTask(ActionEvent actionEvent) throws Exception {
+        DateTimeFormatter timeFormat = DateTimeFormatter.ofPattern("HH:mm:ss");
+        TemporalAccessor timeX = timeFormat.parse(time.getText());
+        if (!time.getText().equals(timeFormat.format(timeX))) {
+            System.out.println("ignorar");//ERRADO;
+        }
+        if (dateId.getValue() == null || time.getText() == null || taskId.getText() == null||categoryId.getValue() == null || descriptionId.getText() == null ){
+            System.out.println("ignorar");//ERRADO
+        }
         DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("dd/MM/yyyy");
         LocalDate datetmp = dateId.getValue();
         String timeofthistask = time.getText();
@@ -39,16 +54,18 @@ public class TasksController {
         String titleofthistask = taskId.getText();
         String categoryofthistask = String.valueOf(categoryId.getValue());
         String descriptionofthistask = descriptionId.getText();
-        Tasks task = new Tasks(titleofthistask, categoryofthistask, false, false, datentimeofthistask, descriptionofthistask);
-        ArrayList<String> newTaskArray = new ArrayList<String>();
-        newTaskArray.add(task.title);
-        newTaskArray.add(task.category);
-        newTaskArray.add(String.valueOf(task.done));
-        newTaskArray.add(String.valueOf(task.favourite));
-        newTaskArray.add(task.deadline);
-        newTaskArray.add(task.description);
-        JSONObject newTaskObject = new JSONObject();
-        newTaskObject.put("Task:", newTaskArray);
+        int identification = (int) (new Date().getTime()/1000);
+
+        Tasks task = new Tasks(identification, titleofthistask, categoryofthistask, false, false, datentimeofthistask, descriptionofthistask, false);
+        newTaskObject.put("id", String.valueOf(task.id));
+        newTaskObject.put("title", task.title);
+        newTaskObject.put("category", task.category);
+        newTaskObject.put("done", String.valueOf(task.done));
+        newTaskObject.put("favourite", String.valueOf(task.favourite));
+        newTaskObject.put("deadline", task.deadline);
+        newTaskObject.put("description", task.description);
+        newTaskObject.put("deleted", String.valueOf(task.deleted));
+
         try (FileWriter file = new FileWriter("tasksList.json", true)) {
             file.write(newTaskObject.toString());
             file.flush();
--- src/main/java/app/models/Category.java
+++ src/main/java/app/models/Category.java
@@ -1,11 +1,9 @@
 package app.models;
 
-import app.models.User;
-
 public abstract class Category{
-    private String type;
-    private String icon;
-    private String description;
+    public String type;
+    public String icon;
+    public String description;
     private User user;
 
 
