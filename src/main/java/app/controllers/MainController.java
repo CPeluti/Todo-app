7912137037341624beec500b515ae61afc168d2a
AnaJordao <acjpjvjp@gmail.com>
2021-05-01 19:52:11 -0300

    Criacao da barra superior onde tem o botao de fechar o app

--- src/main/java/app/controllers/MainController.java
+++ src/main/java/app/controllers/MainController.java
@@ -1,7 +1,18 @@
 package app.controllers;
 
+import app.Main;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+
 public class MainController {
     public void onClickAddBtn(){}
 
+    public Label btnCloseApp;
+
+    @FXML
+    void btnCloseApp(MouseEvent event) {
+        System.exit(0);
+    }
 
 }
